2ea799dc39444104d69bda0b16b1e1cc6d2d2528
ICT DEVS <Maghanga97>
2022-03-24 14:09:36 +0300

    another commit

--- cpccs/settings.py
+++ cpccs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%zvi$jt63pm^c@&d5-@u3m6r$cso^24=tn3k#p_fo5^#b*)omq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cpccs.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['', '127.0.0.1']
 
 
 # Application definition
